0950431c2fe46e96b0151381202b4e61647b89d6
91986 <91986@LAPTOP-AEECM1P8>
2024-07-20 10:59:08 +0530

    Login Test cases written by Suraj Automation Test Engineer

--- src/test/java/Tutorial1/Demo1.java
+++ src/test/java/Tutorial1/Demo1.java
@@ -3,7 +3,8 @@ package Tutorial1;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("BDD Framework is created");
+	
 
 	}
 
